4c8fb37c3e61d3b71865c49942458243ed2497a5
LuKaSzKoWi <138697364+LuKaSzKoWi@users.noreply.github.com>
2024-09-15 14:58:32 +0000

    Deploying to gh-pages from @ LuKaSzKoWi/goit-js-hw-11@f2faf83923f344b4433b098a06eeec7f63ce2855 🚀

--- page-2.js
+++ page-2.js
@@ -1,4 +1,4 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as a,a as n}from"./assets/vendor-s8LL8a4v.js";const i=document.getElementById("search-form"),s=document.getElementById("gallery"),l="45947467-1fb23e21d26a094164d331d1f";i.addEventListener("submit",async t=>{t.preventDefault();const e=document.getElementById("query").value.trim();if(!e){a.error({title:"Error",message:"Please enter a search term!"});return}try{const r=(await n.get(`https://pixabay.com/api/?key=${l}&q=${encodeURIComponent(e)}&image_type=photo&orientation=horizontal&safesearch=true`)).data.hits;if(r.length===0){a.info({message:"Sorry, there are no images matching your search query. Please try again!"}),s.innerHTML="";return}m(r)}catch{a.error({title:"Error",message:"Something went wrong. Please try again later."}),c()}});function m(t){s.innerHTML=t.map(e=>`
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as a,a as i}from"./assets/vendor-s8LL8a4v.js";const l=document.getElementById("search-form"),t=document.getElementById("gallery"),c="45947467-1fb23e21d26a094164d331d1f";l.addEventListener("submit",async r=>{r.preventDefault();const e=document.getElementById("query").value.trim();if(!e){a.error({title:"Error",message:"Please enter a search term!"});return}m(),p();try{const o=(await i.get(`https://pixabay.com/api/?key=${c}&q=${encodeURIComponent(e)}&image_type=photo&orientation=horizontal&safesearch=true`)).data.hits;if(n(),o.length===0){a.info({message:"Sorry, there are no images matching your search query. Please try again!"}),t.innerHTML="";return}d(o)}catch{a.error({title:"Error",message:"Something went wrong. Please try again later."}),n()}});function d(r){t.innerHTML=r.map(e=>`
       <a href="${e.largeImageURL}" class="gallery-item">
         <img src="${e.webformatURL}" alt="${e.tags}" />
         <div class="info">
@@ -7,5 +7,5 @@ import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                
           <p>Comments: ${e.comments}</p>
           <p>Downloads: ${e.downloads}</p>
         </div>
-      </a>`).join("")}function c(){loader.classList.add("hidden")}
+      </a>`).join("")}function m(){t.innerHTML=""}function p(){loader.classList.remove("hidden")}function n(){loader.classList.add("hidden")}
 //# sourceMappingURL=page-2.js.map
