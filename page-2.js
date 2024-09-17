1f1dd2e45e91cf767a13069704a6fdbd749dd6bf
LuKaSzKoWi <138697364+LuKaSzKoWi@users.noreply.github.com>
2024-09-17 07:32:33 +0000

    Deploying to gh-pages from @ LuKaSzKoWi/goit-js-hw-11@c8a008dcc8eb94e251eb77666af3d920c28b745d 🚀

--- page-2.js
+++ page-2.js
@@ -1,4 +1,4 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as a,a as i}from"./assets/vendor-s8LL8a4v.js";const l=document.getElementById("search-form"),t=document.getElementById("gallery"),c="45947467-1fb23e21d26a094164d331d1f";l.addEventListener("submit",async r=>{r.preventDefault();const e=document.getElementById("query").value.trim();if(!e){a.error({title:"Error",message:"Please enter a search term!"});return}m(),p();try{const o=(await i.get(`https://pixabay.com/api/?key=${c}&q=${encodeURIComponent(e)}&image_type=photo&orientation=horizontal&safesearch=true`)).data.hits;if(n(),o.length===0){a.info({message:"Sorry, there are no images matching your search query. Please try again!"}),t.innerHTML="";return}d(o)}catch{a.error({title:"Error",message:"Something went wrong. Please try again later."}),n()}});function d(r){t.innerHTML=r.map(e=>`
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as a,a as l,S as c}from"./assets/vendor-u8rapaCG.js";const m=document.getElementById("search-form"),r=document.getElementById("gallery"),s=document.getElementById("loader"),d="45947467-1fb23e21d26a094164d331d1f";m.addEventListener("submit",async t=>{t.preventDefault();const e=document.getElementById("query").value.trim();if(!e){a.error({title:"Error",message:"Please enter a search term!"});return}g(),h();try{const o=(await l.get(`https://pixabay.com/api/?key=${d}&q=${encodeURIComponent(e)}&image_type=photo&orientation=horizontal&safesearch=true`)).data.hits;if(n(),o.length===0){a.info({message:"Sorry, there are no images matching your search query. Please try again!"}),r.innerHTML="";return}p(o)}catch{a.error({title:"Error",message:"Something went wrong. Please try again later."}),n()}});function p(t){r.innerHTML=t.map(e=>`
       <a href="${e.largeImageURL}" class="gallery-item">
         <img src="${e.webformatURL}" alt="${e.tags}" />
         <div class="info">
@@ -7,5 +7,5 @@ import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                
           <p>Comments: ${e.comments}</p>
           <p>Downloads: ${e.downloads}</p>
         </div>
-      </a>`).join("")}function m(){t.innerHTML=""}function p(){loader.classList.remove("hidden")}function n(){loader.classList.add("hidden")}
+      </a>`).join(""),y()}function g(){r.innerHTML=""}function h(){s.classList.remove("hidden")}function n(){s.classList.add("hidden")}function y(){new c(".gallery-item",{captionsData:"alt",captionDelay:250}).refresh()}
 //# sourceMappingURL=page-2.js.map
